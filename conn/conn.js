d2fddee9ea77973ce172084c450d051eed048b0a
Miller-Oliveira <56763078+Miller-Oliveira@users.noreply.github.com>
2021-12-01 19:04:50 -0300

    Add files via upload

---
+++ conn/conn.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose');
+
+const Conn = (url, user, pass, data) => {
+  mongoose.connect(`${url}/${data}`, {
+      user: user,
+      pass: pass,
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+  }
+).then(()=>{
+  console.log('Conectado, uhuuul!!!');
+}).catch((err)=>{
+  console.error(err);
+})
+}
+
+module.exports = Conn;
